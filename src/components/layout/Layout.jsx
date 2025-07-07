2ad472fc3e0bdefa3a3c25e8c29e07729dbddd7d
Sanathsenthilkumar <sanathsenthilkumar@gmail.com>
2025-07-07 15:30:19 +0530

    initial commit

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,6 +5,7 @@ import Footer from './Footer';
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
+      
       {/* 
         Layout Component
         
