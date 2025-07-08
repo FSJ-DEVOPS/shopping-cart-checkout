69f7e61a5288a4654569e166347522faf18d64ae
Sanathsenthilkumar <166885449+Sanathsenthilkumar@users.noreply.github.com>
2025-07-08 09:45:54 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Order from './pages/Order';
 function App() {
   return (
     <Router>
-      <Routes>git
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/product/:id" element={<ProductDetail />} />
